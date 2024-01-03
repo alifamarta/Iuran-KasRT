374beb2e3a16c9ff5689ec47b0896a5be2aa1be7
Hilman_Amr <hilmanamr22@gmail.com>
2024-01-03 12:11:11 +0700

    commit

--- pages/tambah_iuran.php
+++ pages/tambah_iuran.php
@@ -6,9 +6,144 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulir Data Iuran</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <style>
+        body {
+            background-color: #f8f9fa;
+        }
+
+        .container {
+            background-color: #ffffff;
+            border-radius: 10px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            padding: 30px;
+            margin-top: 50px;
+        }
+
+        .mb-3 {
+            margin-bottom: 15px;
+        }
+        /* Impport font */
+@import url('https://fonts.googleapis.com/css2?family=Istok+Web&display=swap');
+
+body {
+    font-family: 'Istok Web', sans-serif;
+}
+
+.sidebar {
+    height: 100%;
+    width: 0;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    background-color: #212529;
+    overflow-x: hidden;
+    padding-top: 60px;
+    transition: 0.5s;
+}
+
+.sidebar a {
+    padding: 8px 8px 8px 32px;
+    text-decoration: none;
+    font-size: 20px;
+    color: #818181;
+    display: block;
+    transition: 0.3s;
+}
+
+.sidebar a:hover {
+    color: #f1f1f1;
+}
+
+.sidebar .active {
+    background-color: #118eea;
+    color: white;
+}
+
+.sidebar .close {
+    position: absolute;
+    top: 0px;
+    right: 5px;
+    font-size: 30px;
+    margin-left: 50px;
+}
+
+.sidebar-toggle {
+    font-size: 25px;
+    cursor: pointer;
+    background: none;
+    color: #fff;
+    border: none;
+    padding: 10px 15px;
+    margin-left: 10px;
+}
+
+.divider {
+    margin-left: auto;
+    margin-right: auto;
+    color: #f1f1f1;
+    width: 90%;
+}
+
+.sidebar-toggle:hover {
+    color: #dedcdc;
+}
+
+.nav-content .navbar-brand {
+    margin-left: 30px;
+}
+
+.card {
+    width: 23%;
+    height: 200px;
+    color: white;
+    border-radius: 20px;
+    cursor: pointer;
+}
+
+.bg-user {
+    background: url('../media/card_user.png');
+    background-size: contain;
+}
+
+.bg-kasWarga {
+    background: url('../media/card_daftarkas.png');
+    background-size: contain;
+}
+
+.card-kasWarga,
+.card-kas {
+    margin-left: 80px;
+}
+
+.bg-kas {
+    background: url('../media/card_kas.png');
+    background-size: contain;
+}
+
+@media screen and (max-width: 800px) {
+    .card {
+        width: 90%;
+        margin-left: 30px;
+    }
+
+    .card-user,
+    .card-kasWarga,
+    .card-kas {
+        margin-bottom: 30px;
+    }
+}
+    </style>
 </head>
 
 <body>
+     <!-- Tampilan navbar dengan tombol burger -->
+     <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #118eea;">
+        <div class="nav-content">
+            <button class="sidebar-toggle" onclick="openNav()">&#9776;</button>
+            <a href="#" class="navbar-brand">Aplikasi Iuran Kas</a>
+        </div>
+    </nav>
 <div class="container mt-5">
     <h2 class="mb-4">Formulir Data Iuran</h2>
     <form action="../scripts/proses_tambah_iuran.php" method="post">
