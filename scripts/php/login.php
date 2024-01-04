a50639bc67b39b3ca36f0a71f7337637a72b059e
Hilman_Amr <hilmanamr22@gmail.com>
2024-01-04 17:28:35 +0700

    commit

--- scripts/php/login.php
+++ scripts/php/login.php
@@ -7,7 +7,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = "root";
     $password = "";
     $database = "db_kas_rt";
-
     $conn = new mysqli($host, $username, $password, $database);
 
     // Periksa koneksi
